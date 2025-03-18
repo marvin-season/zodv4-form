cd212db247158ac15304cfc477bd77043a01da0f
marvin <sea-son@foxmail.com>
2025-03-18 10:40:52 +0800

    pretty

--- lib/main.tsx
+++ lib/main.tsx
@@ -3,7 +3,12 @@ import { ModalHelperProvider, UseCase } from "./main.ts";
 import "./main.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <ModalHelperProvider
-        modalCustomizeRender={({ modal, closeModal, confirmModal, loading }) => {
+        modalCustomizeRender={({
+            modal,
+            closeModal,
+            confirmModal,
+            loading,
+        }) => {
             return (
                 <div>
                     {modal.headerRender?.({ closeModal, title: modal.title })}
