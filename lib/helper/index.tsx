15a7a908a815af587efdeaa41e57a2c744c5d09e
Marvin <marvin@mail.pollo.ai>
2025-10-14 21:11:04 +0800

    fix: clean

--- lib/helper/index.tsx
+++
@@ -1,2 +0,0 @@
-export { default as ModalHelperProvider, useModalHelper } from "./context";
-export { default as UseCase } from "./use-case";
--- lib/main.ts
+++ lib/main.ts
@@ -1,5 +1,1 @@
-export {
-    default as ModalHelperProvider,
-    useModalHelper,
-} from "@/helper/context";
-export { default as UseCase } from "@/helper/use-case";
+
